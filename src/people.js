3a259af0f866b84a260857ff8cb75b6a44a7e296
Mihai <dughilamihai@gmail.com>
2022-02-21 17:07:13 +0200

    add Get, Post to the API

---
+++ src/people.js
@@ -0,0 +1,13 @@
+export let people = [{
+    name: "will", 
+    age: 35, 
+    hairColor: "brown",
+}, {
+    name: "mike", 
+    age: 40, 
+    hairColor: "brown",
+}, {
+    name: "jogn", 
+    age: 20, 
+    hairColor: "blonde",
+}]
